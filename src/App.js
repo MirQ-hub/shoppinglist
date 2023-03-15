4593d7bc83f2973ee81995eb7c9b73f4f907c05e
MirQ-hub <n2vami00@students.oamk.fi>
2023-03-15 17:17:32 +0200

    map ongelma

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import{useEffect, useState}from 'react';
+
+const URL ="http://localhost/shoppinglist/";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [item, setItem] = useState('');
+
+  function save(e) {
+    e.preventDefault();
+    const json = JSON.stringify({description:item})
+    axios.post(URL +'add.php',json,{
+      headers:{
+        'Content-Type' : 'application/json'
+      }
+    })
+    .then((response) =>{
+      setItems(items =>[...items,response.data]);
+      setItem('');
+    }).catch(error => {
+      alert(error.response.data.error);
+    })
+  }
+  
+
+
+  useEffect(()=> {
+    axios.get(URL)
+    .then((response) => {
+      setItems(response.data)
+    }).catch(error =>{
+      alert(error.response ? error.response.data.error : error);
+    })
+  } ,[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h3>Shoppinglist</h3>
+      <form onSubmit={save}>
+        <label>New item</label>
+        <input value={item} onChange={e =>setItem(e.target.value)}/>
+        <button>Save</button>
+      </form>
+      <ol>
+        {items.map((item) => (
+          <li key={item.id}>{item.description}</li>
+        ))}
+      </ol>
     </div>
   );
 }
